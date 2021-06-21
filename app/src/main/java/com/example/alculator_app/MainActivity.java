b21c7f44df69e8eead7dd71a664d4cfa5f1cfb32
Vera <ipeachyou@yandex.ru>
2021-06-21 20:58:00 +0500

    отформатировала activity main. Добавила горизональную версию.
    Решила перенести стиль кнопок в файл style, потому что стиль стал занимать слишком много места.

--- app/src/main/java/com/example/alculator_app/MainActivity.java
+++ app/src/main/java/com/example/alculator_app/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.alculator_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Display;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState==null){
+            makeToast("первый запуск onCreate");
+        }else{
+            makeToast("повторный запуск onCreate");
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        makeToast("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy");
+    }
+
+    private void makeToast(String toast){
+        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+        Log.e("myLogs", toast);
+    }
+
 }
