613ee27a07fa3228e0c6fb93caf5d038c3a5cebb
Vera <ipeachyou@yandex.ru>
2021-06-21 22:52:40 +0500

    отформатировала activity main.
    Добавила новую кнопку

--- app/src/main/java/com/example/alculator_app/MainActivity.java
+++ app/src/main/java/com/example/alculator_app/MainActivity.java
@@ -18,52 +18,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(savedInstanceState==null){
-            makeToast("первый запуск onCreate");
-        }else{
-            makeToast("повторный запуск onCreate");
-        }
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        makeToast("onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        makeToast("onPause");
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        makeToast("onSaveInstanceState");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        makeToast("onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        makeToast("onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        makeToast("onDestroy");
-    }
-
-    private void makeToast(String toast){
-        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
-        Log.e("myLogs", toast);
-    }
-
 }
