3d20e3a00d5c30c60a32da975d137441dd8e92cd
Vera <ipeachyou@yandex.ru>
2021-06-23 21:38:59 +0500

    добавила обработку кнопок. немного исправила внешний вид калькулятора.
    он пока не умеет работать с дробными числвми, кнопка point отключена

---
+++ app/src/main/java/com/example/alculator_app/CalculatorActivity.java
@@ -0,0 +1,77 @@
+package com.example.alculator_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.DigitsKeyListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    private CalculatorModel calculator;
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int[] numberIds = new int[] {
+                R.id.zero,
+                R.id.one,
+                R.id.two,
+                R.id.three,
+                R.id.four,
+                R.id.five,
+                R.id.six,
+                R.id.seven,
+                R.id.eight,
+                R.id.nine,
+                /*R.id.point*/
+        };
+
+        int[] actionsIds = new int[] {
+                R.id.plus,
+                R.id.minus,
+                R.id.multiply,
+                R.id.division,
+                R.id.equals
+        };
+
+        text = findViewById(R.id.text);
+        calculator = new CalculatorModel();
+
+
+        View.OnClickListener numberButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculator.onNumPressed(view.getId());
+                text.setText(calculator.getText());
+            }
+        };
+
+        View.OnClickListener actionButtonOnclickListener = view -> {
+            calculator.onActionPressed(view.getId());
+            text.setText(calculator.getText());
+        };
+
+        for (int numberId : numberIds) {
+            findViewById(numberId).setOnClickListener(numberButtonClickListener);
+        }
+
+        for (int actionsId : actionsIds) {
+            findViewById(actionsId).setOnClickListener(actionButtonOnclickListener);
+        }
+
+        findViewById(R.id.reset).setOnClickListener(view -> {
+            calculator.reset();
+            text.setText(calculator.getText());
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/alculator_app/CalculatorModel.java
@@ -0,0 +1,148 @@
+package com.example.alculator_app;
+
+import androidx.constraintlayout.solver.state.State;
+
+public class CalculatorModel {
+
+    private int first_number;
+    private int second_number;
+    private StringBuilder input_string = new StringBuilder();
+    private int selected_action;
+    private State state;
+
+    private enum State {
+        first_number_input,
+        operation_selected,
+        second_number_input,
+        show_result
+    }
+
+    public CalculatorModel() {
+        state = State.first_number_input;
+    }
+
+    public void onNumPressed(int buttonId) {
+
+        if (state == State.show_result) {
+            state = State.first_number_input;
+            input_string.setLength(0);
+        }
+
+        if (state == State.operation_selected) {
+            state = State.second_number_input;
+            input_string.setLength(0);
+        }
+        if (input_string.length() < 9) {
+            switch (buttonId) {
+                case R.id.zero:
+                    if (input_string.length() != 0) {
+                        input_string.append("0");
+                    }
+                    break;
+                case R.id.one:
+                    input_string.append("1");
+                    break;
+                case R.id.two:
+                    input_string.append("2");
+                    break;
+                case R.id.three:
+                    input_string.append("3");
+                    break;
+                case R.id.four:
+                    input_string.append("4");
+                    break;
+                case R.id.five:
+                    input_string.append("5");
+                    break;
+                case R.id.six:
+                    input_string.append("6");
+                    break;
+                case R.id.seven:
+                    input_string.append("7");
+                    break;
+                case R.id.eight:
+                    input_string.append("8");
+                    break;
+                case R.id.nine:
+                    input_string.append("9");
+                    break;
+                case R.id.point:
+                    input_string.append(".");
+                    break;
+            }
+        }
+    }
+
+    public void onActionPressed(int actionId) {
+        if (actionId == R.id.equals && state == State.second_number_input && input_string.length() > 0) {
+            second_number = Integer.parseInt(input_string.toString());
+            state = State.show_result;
+            input_string.setLength(0);
+            switch ((int) selected_action) {
+                case R.id.plus:
+                    input_string.append(first_number + second_number);
+                    break;
+                case R.id.minus:
+                    input_string.append(first_number - second_number);
+                    break;
+                case R.id.multiply:
+                    input_string.append(first_number * second_number);
+                    break;
+                case R.id.division:
+                    input_string.append(first_number / second_number);
+                    break;
+            }
+        } else if (input_string.length() > 0 && state == State.first_number_input) {
+            first_number = Integer.parseInt(input_string.toString());
+            state = State.operation_selected;
+            selected_action = actionId;
+        }
+    }
+
+    public String getText() {
+        StringBuilder str = new StringBuilder();
+        switch (state) {
+            default:
+                return input_string.toString();
+            case operation_selected:
+                return str.append(first_number).append(' ')
+                        .append(getOperationChar())
+                        .toString();
+            case second_number_input:
+                return str.append(first_number).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(input_string)
+                        .toString();
+            case show_result:
+                return str.append(first_number).append(' ')
+                        .append(getOperationChar())
+                        .append(' ')
+                        .append(second_number)
+                        .append(" = ")
+                        .append(input_string.toString())
+                        .toString();
+        }
+    }
+
+    private char getOperationChar() {
+        switch ((int) selected_action) {
+            case R.id.plus:
+                return '+';
+            case R.id.minus:
+                return '-';
+            case R.id.multiply:
+                return '*';
+            case R.id.division:
+            default:
+                return '/';
+
+        }
+    }
+
+    public void reset() {
+        state = State.first_number_input;
+        input_string.setLength(0);
+    }
+
+}
